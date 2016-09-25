10bae164af032f01546442eaba18bbb5c654fcf6
aytona <christopher.aytona@gmail.com>
2016-09-24 23:04:42 -0500

    base organism class

---
+++ src/com/chrisaytona/Organism.java
@@ -0,0 +1,24 @@
+package com.chrisaytona;
+
+public class Organism
+{
+    public void move()
+    {
+
+    }
+
+    public void breed()
+    {
+
+    }
+}
+
+class Ant extends Organism
+{
+
+}
+
+class Doodlebug extends Organism
+{
+
+}
