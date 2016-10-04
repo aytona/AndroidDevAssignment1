321ca7c9cb4527c92483565fd9e253f9cdc02400
Christopher Aytona <christopher.aytona@gmail.com>
2016-10-04 13:37:25 -0400

    Removed unnecessary int

--- src/com/chrisaytona/Organism.java
+++ src/com/chrisaytona/Organism.java
@@ -4,12 +4,10 @@ package com.chrisaytona;
 public class Organism
 {
     private String name;
-    private int currentLifeStep;
 
     public Organism()
     {
         this.name = "";
-        this.currentLifeStep = 0;
     }
 
     private void move(Organism[] grid, int width)
